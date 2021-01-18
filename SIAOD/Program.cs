7b69a190e72403e72b9f318c79adc37d31c6210a
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2021-01-18 14:51:31 +0300

    refactor

--- SIAOD/Program.cs
+++ SIAOD/Program.cs
@@ -6,8 +6,6 @@ namespace SIAOD
     //1.  Выполнить симметричную прошивку бинарного дерева поиска.Обойти его согласно симметричному порядку следования элементов. Реализовать вставку и удаление элементов из симметрично прошитого бинарного дерева.
     //2.  Выполнить прямую прошивку бинарного дерева поиска. Обойти его согласно прямому порядку следования элементов.Реализовать вставку и удаление элементов из прямо прошитого бинарного дерева.
 
-
-
     class Program
     {
         //класс узла - наш объект в стеке
@@ -25,7 +23,7 @@ namespace SIAOD
             public bool isNoThreadRight;
         }
 
-        //Прошитое бинароное дерево поиска
+        //Прошитое бинарное дерево поиска
         public unsafe class Tree
         {
             // Вставка
@@ -89,51 +87,188 @@ namespace SIAOD
                 return rootNode;
             }
 
-            // возвращает наследника использую правую ветку
-            private Node* OrderSuccessor(Node* ptr)
+
+
+            // обход дерева для вывода
+            public void TreeTraversal(Node* root)
             {
-                // идем в правое поддерево
-                if (ptr->isNoThreadRight)
+                // Возвращает наследника
+                Node* getSuccessor(Node* ptr)
                 {
-                    return ptr->right;
+                    // идем в правое поддерево
+                    if (ptr->isNoThreadRight)
+                    {
+                        return ptr->right;
+                    }
+
+                    // идем в левое поддерево
+                    ptr = ptr->right;
+                    while (ptr->isNoThreadLeft == false)
+                    {
+                        ptr = ptr->left;
+                    }
+
+                    return ptr;
                 }
-
-                // идем в левое поддерево
-                ptr = ptr->right;
-                while (ptr->isNoThreadLeft == false)
-                {
-                    ptr = ptr->left;
-                }
-
-                return ptr;
-            }
-
-            // выводимм дерево
-            public void OrderTraversal(Node* root)
-            {
+                
                 if (root == null)
-                {
                     return;
-                }
 
-                // находим левую ноду
-                Node* ptr = root;
+                var ptr = root;
                 while (ptr->isNoThreadLeft == false)
                 {
                     ptr = ptr->left;
                 }
 
-                // печатаем
+                // цикл для вывода информации
                 while (ptr != null)
                 {
                     Console.Write(ptr->data + " ");
-                    ptr = OrderSuccessor(ptr);
+                    ptr = getSuccessor(ptr);
                 }
             }
 
             // удаление 
             public Node* RemoveNode(Node* rootNode, int data)
             {
+
+                // левый лист удаляем
+                Node* getNodeWhenNoSuccessors(Node* root, Node* par, Node* ptr)
+                {
+                    // если нода для удаления вершина
+                    if (par == null)
+                        root = null;
+
+                    // левая или родитель
+                    else if (ptr == par->left)
+                    {
+                        par->isNoThreadLeft = true;
+                        par->left = ptr->left;
+                    }
+                    else
+                    {
+                        par->isNoThreadRight = true;
+                        par->right = ptr->right;
+                    }
+
+                    return root;
+                }
+
+                // у узла два наследника
+                Node* getNodeWhenTwoSuccessors(Node* root, Node* ptr)
+                {
+                    // находим родителя
+                    Node* parSuccessor = ptr;
+                    Node* successor = ptr->right;
+
+                    // находим левого потомка наследника
+                    while (successor->isNoThreadLeft == false)
+                    {
+                        parSuccessor = successor;
+                        successor = successor->left;
+                    }
+
+                    ptr->data = successor->data;
+
+                    if (successor->isNoThreadLeft == true && successor->isNoThreadRight == true)
+                    {
+                        root = getNodeWhenNoSuccessors(root, parSuccessor, successor);
+                    }
+                    else
+                    {
+                        root = getNodeWhenOneSuccessors(root, parSuccessor, successor);
+                    }
+
+                    return root;
+                }
+
+                // один потомок
+                Node* getNodeWhenOneSuccessors(Node* root, Node* par, Node* ptr)
+                {
+
+                    // ищем преемника
+                    Node* findSuccessor(Node* ptr)
+                    {
+                        if (ptr->isNoThreadRight)
+                        {
+                            return ptr->right;
+                        }
+
+                        ptr = ptr->right;
+                        while (ptr->isNoThreadLeft == false)
+                        {
+                            ptr = ptr->left;
+                        }
+
+                        return ptr;
+                    }
+
+                    // ищем предшественника 
+                    Node* findPred(Node* ptr)
+                    {
+                        if (ptr->isNoThreadLeft == true)
+                        {
+                            return ptr->left;
+                        }
+
+                        ptr = ptr->left;
+                        while (ptr->isNoThreadRight == false)
+                        {
+                            ptr = ptr->right;
+                        }
+
+                        return ptr;
+                    }
+
+                    Node* child;
+
+                    // только левый потомок
+                    if (ptr->isNoThreadLeft == false)
+                    {
+                        child = ptr->left;
+                    }
+                    // только правый потомок
+                    else
+                    {
+                        child = ptr->right;
+                    }
+
+                    // вершина
+                    if (par == null)
+                    {
+                        root = child;
+                    }
+                    // нода для удаления левый ребенок родителя
+                    else if (ptr == par->left)
+                    {
+                        par->left = child;
+                    }
+                    else
+                    {
+                        par->right = child;
+                    }
+
+                    // находим приемника и предшественника
+                    var s = findSuccessor(ptr);
+                    var p = findPred(ptr);
+
+                    // есть правая ветка
+                    if (ptr->isNoThreadLeft == false)
+                    {
+                        p->right = s;
+                    }
+                    // есть правая ветка
+                    else
+                    {
+                        if (ptr->isNoThreadRight == false)
+                        {
+                            s->left = p;
+                        }
+                    }
+
+                    return root;
+                }
+
                 var isFoundElement = false;
                 Node* par = null, ptr = rootNode;
 
@@ -151,7 +286,7 @@ namespace SIAOD
                     if (data < ptr->data)
                     {
                         if (ptr->isNoThreadLeft == false)
-                            //устанавливаем указатель на левого потомка
+                            // указатель на левого потомка
                             ptr = ptr->left;
                         else
                             break;
@@ -159,7 +294,7 @@ namespace SIAOD
                     else
                     {
                         if (ptr->isNoThreadRight == false)
-                            //устанавливаем указатель на правого потомка
+                            // указатель на правого потомка
                             ptr = ptr->right;
                         else
                             break;
@@ -170,169 +305,28 @@ namespace SIAOD
                 {
                     Console.WriteLine("Нет искомого элемента");
                 }
-                // ситуация когда два потомка
+                //  есть ва потомка
                 else if (ptr->isNoThreadLeft == false && ptr->isNoThreadRight == false)
                 {
-                    rootNode = caseC(rootNode, ptr);
+                    rootNode = getNodeWhenTwoSuccessors(rootNode, ptr);
                 }
-                // только левый потом
+                // есть левый потом
                 else if (ptr->isNoThreadLeft == false)
                 {
-                    rootNode = caseB(rootNode, par, ptr);
+                    rootNode = getNodeWhenOneSuccessors(rootNode, par, ptr);
                 }
-                // только правый потомок
+                // есть правый потомок
                 else if (ptr->isNoThreadRight == false)
                 {
-                    rootNode = caseB(rootNode, par, ptr);
+                    rootNode = getNodeWhenOneSuccessors(rootNode, par, ptr);
                 }
-                // нету потомков
+                // потомков нет
                 else
                 {
-                    rootNode = caseA(rootNode, par, ptr);
+                    rootNode = getNodeWhenNoSuccessors(rootNode, par, ptr);
                 }
 
                 return rootNode;
-            }
-
-            // удаление левого листа
-            public Node* caseA(Node* root, Node* par, Node* ptr)
-            {
-                // если нода для удаления вершина
-                if (par == null)
-                    root = null;
-
-                // левая или родитель
-                else if (ptr == par->left)
-                {
-                    par->isNoThreadLeft = true;
-                    par->left = ptr->left;
-                }
-                else
-                {
-                    par->isNoThreadRight = true;
-                    par->right = ptr->right;
-                }
-
-                return root;
-            }
-
-            // нода для удаления имеет только одного потомка
-            private Node* caseB(Node* root, Node* par,
-                Node* ptr)
-            {
-
-                // находим преемника
-                Node* inSucc(Node* ptr)
-                {
-                    if (ptr->isNoThreadRight)
-                    {
-                        return ptr->right;
-                    }
-
-                    ptr = ptr->right;
-                    while (ptr->isNoThreadLeft == false)
-                    {
-                        ptr = ptr->left;
-                    }
-
-                    return ptr;
-                }
-
-                // находим предшественника 
-                Node* inPred(Node* ptr)
-                {
-                    if (ptr->isNoThreadLeft == true)
-                    {
-                        return ptr->left;
-                    }
-
-                    ptr = ptr->left;
-                    while (ptr->isNoThreadRight == false)
-                    {
-                        ptr = ptr->right;
-                    }
-
-                    return ptr;
-                }
-                
-                Node* child;
-
-                // имеет только левого потомка
-                if (ptr->isNoThreadLeft == false)
-                {
-                    child = ptr->left;
-                }
-                // только правый потомок
-                else
-                {
-                    child = ptr->right;
-                }
-
-                // вершина
-                if (par == null)
-                {
-                    root = child;
-                }
-                // нода для удаления левый ребенок родителя
-                else if (ptr == par->left)
-                {
-                    par->left = child;
-                }
-                else
-                {
-                    par->right = child;
-                }
-
-                // находим приемника и предшественника
-                var s = inSucc(ptr);
-                var p = inPred(ptr);
-
-                // If ptr has left subtree.
-                if (ptr->isNoThreadLeft == false)
-                {
-                    p->right = s;
-                }
-                // если существует правая ветка
-                else
-                {
-                    if (ptr->isNoThreadRight == false)
-                    {
-                        s->left = p;
-                    }
-                }
-
-                return root;
-            }
-
-
-
-
-            // ситуация когда у ноды для удаления два наследника
-            Node* caseC(Node* root, Node* ptr)
-            {
-                // находим родителя
-                Node* parsucc = ptr;
-                Node* succ = ptr->right;
-
-                // находим левого потомка наследника
-                while (succ->isNoThreadLeft == false)
-                {
-                    parsucc = succ;
-                    succ = succ->left;
-                }
-
-                ptr->data = succ->data;
-
-                if (succ->isNoThreadLeft == true && succ->isNoThreadRight == true)
-                {
-                    root = caseA(root, parsucc, succ);
-                }
-                else
-                {
-                    root = caseB(root, parsucc, succ);
-                }
-
-                return root;
             }
         }
 
@@ -340,15 +334,14 @@ namespace SIAOD
         {
             var tree = new Tree();
 
-            // создание узлов
-            var root = new Node { data = 20, isNoThreadLeft = true, isNoThreadRight = true };
-            var node1 = new Node { data = 10, isNoThreadLeft = true, isNoThreadRight = true };
-            var node2 = new Node { data = 30, isNoThreadLeft = true, isNoThreadRight = true };
-            var node3 = new Node { data = 5, isNoThreadLeft = true, isNoThreadRight = true };
-            var node4 = new Node { data = 16, isNoThreadLeft = true, isNoThreadRight = true };
-            var node5 = new Node { data = 14, isNoThreadLeft = true, isNoThreadRight = true };
-            var node6 = new Node { data = 17, isNoThreadLeft = true, isNoThreadRight = true };
-            var node7 = new Node { data = 13, isNoThreadLeft = true, isNoThreadRight = true };
+            var root = new Node { data = 15, isNoThreadLeft = true, isNoThreadRight = true };
+            var node2 = new Node { data = 25, isNoThreadLeft = true, isNoThreadRight = true };
+            var node1 = new Node { data = 5, isNoThreadLeft = true, isNoThreadRight = true };
+            var node3 = new Node { data = 1, isNoThreadLeft = true, isNoThreadRight = true };
+            var node4 = new Node { data = 11, isNoThreadLeft = true, isNoThreadRight = true };
+            var node5 = new Node { data = 9, isNoThreadLeft = true, isNoThreadRight = true };
+            var node6 = new Node { data = 12, isNoThreadLeft = true, isNoThreadRight = true };
+            var node7 = new Node { data = 8, isNoThreadLeft = true, isNoThreadRight = true };
             
             //добавляем в дерево
             var rootPointer = &root;
@@ -361,13 +354,15 @@ namespace SIAOD
             rootPointer = tree.Insert(rootPointer, &node6);
             rootPointer = tree.Insert(rootPointer, &node7);
 
-            // выводим дерево
-            tree.OrderTraversal(rootPointer);
-            // удаляем 20
-            tree.RemoveNode(rootPointer, 20);
+            Console.WriteLine("Обход первоначальный");
+            tree.TreeTraversal(rootPointer);
             Console.WriteLine();
-            // выводим
-            tree.OrderTraversal(rootPointer);
+
+            Console.WriteLine("Удаляем элемент со значением " + 15);
+            tree.RemoveNode(rootPointer, 15);
+
+            Console.WriteLine("Обход после удаления");
+            tree.TreeTraversal(rootPointer);
 
         }
     }
