ac61837c84e2f2551384c3a63e7d1f1ee89a683e
Ahniya Staravoitava <Ahniya_Staravoitava@epam.com>
2021-01-18 12:12:17 +0300

    refactor

--- SIAOD/Program.cs
+++ SIAOD/Program.cs
@@ -1,246 +1,283 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace SIAOD
 {
-    //1. Используя стек, реализовать алгоритм преобразования алгебраического выражения из инфиксной формы записи в постфиксную форму представления.
-    //2. Используя стек, реализовать алгоритм преобразования алгебраического выражения из инфиксной формы записи в префиксную форму представления.
-    //Для обоих алгоритмов предусмотреть вхождение операций с различными приоритетами, а также наличие скобок в инфиксных выражениях.
+    //Ввести 10-15 целых чисел и построить из них с помощью указателей бинарное дерево поиска.
+    //Обойти его прямым, симметричным и обратным способами.
+    //Реализовать процедуры поиска, вставки и удаления элементов  бинарного дерева поиска.
 
 
     class Program
     {
         //класс узла - наш объект в стеке
-        public class Node<T>
+        public unsafe struct Node
         {
-            // следующий узел
-            public Node<T> next;
-            // данные
-            public T data;
-
-            public Node(T data)
-            {
-                this.data = data;
-            }
+            //данные
+            public int data;
+            //указатали
+            public Node* left;
+            public Node* right;
+            public Node* parent;
         }
 
-        //стек
-        public class Stack<T>
+        public unsafe class Tree
         {
-            //голова стека
-            private Node<T> _head;
-            
-            //количество элементов
-            private int _count;
+            //указатель на вершину дерева
 
-            public bool IsEmpty => _count == 0;
+            public Node* Root { get; private set; }
 
-            //добавление в стек
-            public void Push(T item)
+            //конструктор
+            public Tree()
             {
-                // создаем узел
-                var node = new Node<T>(item)
+                Root = null;
+            }
+
+            public void Add(Node* node)
+            {
+                // дерево пустое
+                //устанавливаем вершину
+                if (Root == null)
                 {
-                    //перемещаем указатель
-                    next = _head
-                };
-                _head = node;
-                //увеличиваем количество
-                _count++;
-            }
-
-            //извлечение из стека
-            public T Pop()
-            {
-                // бросаем исключение при пустом стеке
-                if (IsEmpty)
-                    throw new Exception("В стеке нет элементов.");
-                
-                var node = _head;
-                //перемещаем указатель
-                _head = _head.next;
-                //уменьшаем количество
-                _count--;
-                
-                return node.data;
-            }
-
-            //возрат верхнего элемента из стека
-            public T Peek()
-            {
-                // бросаем исключение при пустом стеке
-                if (IsEmpty)
-                    throw new InvalidOperationException("В стеке нет элементов.");
-                
-                return _head.data;
-            }
-        }
-
-        public static class StackHelpers
-        {
-            //получаем приоритет символа
-            private static int GetPriority(char symbol)
-            {
-                switch (symbol)
-                {
-                    case '(':
-                        return 0;
-                    case '+':
-                    case '-':
-                        return 1;
-                    case '/':
-                    case '*':
-                        return 2;
-                    default:
-                        return -1;
+                    Root = node;
+                    return;
                 }
-            }
 
-            //алгоритм преобразования алгебраического выражения из инфиксной формы записи в постфиксную форму представления
-            internal static string InfixToPrefix(string infix)
-            {
-                var result = string.Empty;
-                
-                // в обратном порядке
-                for (var i = infix.Length - 1; i >= 0; i--)
+                var currentNode = Root;
+                while (node->parent == null)
                 {
-                    result += infix[i] switch
+                    // цикл по левой ветке
+                    if (currentNode->data > node->data)
                     {
-                        ')' => '(',
-                        '(' => ')',
-                        _ => infix[i]
-                    };
-                }
-
-
-                // транслирование
-                var resultArray = InfixToPostfix(result).ToCharArray();
-                
-                //переворачивание
-                Array.Reverse(resultArray);
-                return new string(resultArray);
-            }
-
-            //алгоритм преобразования алгебраического выражения из инфиксной формы записи в префиксную форму представления.
-            internal static string InfixToPostfix(string infix)
-            {
-                static bool IsOperator(char symbol)
-                {
-                    const string stackOperationList = "*/+-()";
-                    return stackOperationList.Contains(symbol);
-                }
-
-                var result = string.Empty;
-                var stack = new Stack<char>();
-
-                foreach (var symbol in infix)
-                {
-                    if (IsOperator(symbol))
-                    {
-                        var priority = GetPriority(symbol);
-                        // стек пустой или нашли открывающуюся скобку
-                        if (stack.IsEmpty || symbol == '(')
+                        // если левая ветка пустая
+                        if (currentNode->left == null)
                         {
-                            //добавляем в стек
-                            stack.Push(symbol);
-                            continue;
+                            currentNode->left = node;
+                            node->parent = currentNode;
                         }
-
-                        var node = stack.Peek();
-                        var nodePriority = GetPriority(node);
-
-                        if (symbol == ')')
-                        {
-                            while (!stack.IsEmpty)
-                            {
-                                if (node == '(')
-                                {
-                                    // выход из цикла
-                                    // когда нашли открывающуюся скобку
-                                    stack.Pop();
-                                    break;
-                                }
-                                //добавляем к результату
-                                result += node;
-                                //извлечение из стека
-                                stack.Pop();
-                                node = stack.Peek();
-                            }
-
-                            continue;
-                        }
-
-                        // когда приоритет меньше 
-                        // добавляем в стек
-                        if (nodePriority < priority)
-                        {
-                            stack.Push(symbol);
-                        }
+                        // переход к другой вершине
                         else
                         {
-                            while (nodePriority >= priority)
-                            {
-                                // когда нашли открывающуюся скобку
-                                //добавляем к результату
-
-                                if (node != '(')
-                                {
-                                    result += node;
-                                }
-                                
-                                //извлечение из стека
-                                stack.Pop();
-                                
-                                //итерация окончена
-                                if (stack.IsEmpty)
-                                {
-                                    break;
-                                }
-
-                                node = stack.Peek();
-                                nodePriority = GetPriority(node);
-                            }
-
-                            stack.Push(symbol);
+                            currentNode = currentNode->left;
                         }
+                    }
+                    // цикл по правой ветке
+                    else
+                    {
+                        // если правая ветка пустая
+                        if (currentNode->right == null)
+                        {
+                            currentNode->right = node;
+                            node->parent = currentNode;
+                        }
+                        // переход к другой вершине
+                        else
+                        {
+                            currentNode = currentNode->right;
+                        }
+                    }
+                }
+            }
+
+            // высота дерева
+            public int GetTreeHeight(Node* root)
+            {
+                if (root == null) return 0;
+
+                // запускаем рекурсию
+                var treeHeightLeft = GetTreeHeight(root->left);
+                var treeHeightRight = GetTreeHeight(root->right);
+
+                return treeHeightLeft > treeHeightRight ? treeHeightLeft + 1 : treeHeightRight + 1;
+            }
+
+            //вывод информации о строке по указанной высота
+            private static void WriteRowInformation(Node* root, int height)
+            {
+                if (root == null) return;
+
+                if (height == 1)
+                {
+                    Console.Write($"{root->data} ");
+                }
+                else if (height > 1)
+                {
+                    //идем в левое поддерево
+                    WriteRowInformation(root->left, height - 1);
+                    //идем в правое поддерево
+                    WriteRowInformation(root->right, height - 1);
+                }
+            }
+
+            // Прямой обход дерева
+            public void PreOrderTraversal(Node* root)
+            {
+                for (var i = 0; i <= GetTreeHeight(root); i++)
+                {
+                    WriteRowInformation(root, i);
+                    Console.WriteLine();
+                }
+            }
+
+            // Обратный обход дерева
+            public void PostOrderTraversal(Node* root)
+            {
+                for (var i = GetTreeHeight(root); i >= 1; i--)
+                {
+                    WriteRowInformation(root, i);
+                    Console.WriteLine();
+                }
+            }
+
+            // Симетричный обход дерева рекурсивно
+            public void SymmetricTreeTraversal(Node* root)
+            {
+                if (root == null) return;
+
+                //идем в левое поддерево
+                SymmetricTreeTraversal(root->left);
+                Console.Write(root->data + " ");
+                //идем в правое поддерево
+                SymmetricTreeTraversal(root->right);
+            }
+
+            public Node* SearchNode(int value)
+            {
+                var currentNode = Root;
+                while (currentNode != null)
+                {
+                    //найден результат
+                    if (currentNode->data == value)
+                    {
+                        return currentNode;
+                    }
+
+                    //идем в левое поддерево
+                    if (currentNode->data > value)
+                    {
+                        currentNode = currentNode->left;
+                    }
+                    //идем в правое поддерево
+                    else
+                    {
+                        currentNode = currentNode->right;
+                    }
+                }
+                //не нашли
+                return null;
+            }
+
+
+
+            public Node* Remove(Node* root, int data)
+            {
+                // поиск минимального через рекурсию
+                static Node* getMinNode(Node* root)
+                {
+                    if (root->left == null)
+                    {
+                        return root;
+                    }
+
+                    return getMinNode(root->left);
+                }
+                
+                if (root == null) return null;
+
+                //идем в левое поддерево
+                if (data < root->data)
+                {
+                    root->left = Remove(root->left, data);
+                }
+                else if (data > root->data)
+                {
+                    root->right = Remove(root->right, data);
+                }
+                else if (root->left != null && root->right != null)
+                {
+                    //искомый  элемент в корне текущего поддерева
+                    root->data = getMinNode(root->right)->data;
+                    root->right = Remove(root->right, root->data);
+                }
+                else
+                {
+                    if (root->left != null)
+                    {
+                        root = Root->left;
+                    }
+                    else if (root->right != null)
+                    {
+                        root = root->right;
                     }
                     else
                     {
-                        //сразу добавляем к результату
-                        result += symbol;
+                        root = null;
                     }
                 }
 
-                while (!stack.IsEmpty)
-                {
-                    // добавляем к результату верхний элемент 
-                    //пока стек не пустой
-                    result += stack.Peek();
-                    stack.Pop();
-                }
-
-                return result;
+                return root;
             }
+
+
         }
 
-
-        static void Main(string[] args)
+        static unsafe void Main(string[] args)
         {
-            var infix1 = "А*В+С*В";
-            Console.WriteLine(infix1);
+            var node1 = new Node { data = 11 };
+            var node2 = new Node { data = 8 };
+            var node3 = new Node { data = 13 };
+            var node4 = new Node { data = 6 };
+            var node5 = new Node { data = 9 };
+            var node6 = new Node { data = 12 };
+            var node7 = new Node { data = 14 };
+            var node8 = new Node { data = 15 };
+            var node9 = new Node { data = 16 };
+            var node10 = new Node { data = 6 };
 
-            var infix2 = "(А+В)*(С+D)";
-            Console.WriteLine(infix2);
+
+            var tree = new Tree();
+            tree.Add(&node1);
+            tree.Add(&node2);
+            tree.Add(&node3);
+            tree.Add(&node4);
+            tree.Add(&node5);
+            tree.Add(&node6);
+            tree.Add(&node7);
+            tree.Add(&node8);
+            tree.Add(&node9);
+            tree.Add(&node10);
+
+            //поиск
+            Console.WriteLine("Поиск элемента по значению: " + 11);
+            var result = tree.SearchNode(11);
+            Console.WriteLine("Левый потомок: " + result->left->data);
+            Console.WriteLine("Правый потомок: " + result->right->data);
+            Console.WriteLine("-----------");
+
+            //удаление
             Console.WriteLine();
+            Console.WriteLine("Дерево:");
+            tree.PreOrderTraversal(tree.Root);
+            Console.WriteLine("Удаление элемента по значению: " + 16);
+            tree.Remove(tree.Root, 16);
+            Console.WriteLine("Дерево:");
+            tree.PreOrderTraversal(tree.Root);
+            Console.WriteLine("Полученная высота: " + tree.GetTreeHeight(tree.Root));
+            Console.WriteLine("-----------");
 
-
-            Console.WriteLine("Prefix: " + StackHelpers.InfixToPrefix(infix1));
-            Console.WriteLine("Postfix: " + StackHelpers.InfixToPostfix(infix1));
-            Console.WriteLine("--------");
+            //обходы
             Console.WriteLine();
+            Console.WriteLine("Прямой обход дерева");
+            tree.PreOrderTraversal(tree.Root);
+            Console.WriteLine("-----------");
 
-            Console.WriteLine("Prefix: " + StackHelpers.InfixToPrefix(infix2));
-            Console.WriteLine("Postfix: " + StackHelpers.InfixToPostfix(infix2));
-            Console.WriteLine("--------");
+            Console.WriteLine("Обратный обход дерева");
+            tree.PostOrderTraversal(tree.Root);
+            Console.WriteLine("-----------");
+
+            Console.WriteLine("Симметричный обход дерева");
+            tree.SymmetricTreeTraversal(tree.Root);
             Console.WriteLine();
 
         }
